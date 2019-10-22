b9a55789a1d2f5c520bff8e4f6770183a67ad856
Cornelius Okeke <okekecornelius@gmail.com>
2019-10-22 17:54:21 +0100

    Setting up angular program

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BanksComponent } from './banks/banks.component';
+import { CustomersComponent } from './customers/customers.component';
+import { StatesComponent } from './states/states.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { MessagesComponent } from './messages/messages.component';
+import { InteractionsComponent } from './interactions/interactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    BanksComponent,
+    CustomersComponent,
+    StatesComponent,
+    TransactionsComponent,
+    MessagesComponent,
+    InteractionsComponent
   ],
   imports: [
     BrowserModule,
